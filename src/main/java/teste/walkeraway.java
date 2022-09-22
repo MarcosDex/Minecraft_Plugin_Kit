465f2cdd3bc6fcd81d45a16e94a6d9eef62a0fc1
WalkerAway <54985051+MarcosDex@users.noreply.github.com>
2022-09-22 07:57:56 -0300

    Update walkeraway.java

--- src/main/java/teste/walkeraway.java
+++ src/main/java/teste/walkeraway.java
@@ -1,11 +1,5 @@
 package teste;
 
-//import org.bukkit.Material;
-//import org.bukkit.command.Command;
-//import org.bukkit.command.CommandSender;
-//import org.bukkit.entity.Player;
-//import org.bukkit.inventory.ItemStack;
-
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class walkeraway extends JavaPlugin {
@@ -22,37 +16,10 @@ public class walkeraway extends JavaPlugin {
 
     }
 
-//   @Override
-//    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-//        if (sender instanceof Player) {
-//            Player player = (Player) sender;
-//
-//            // Create a new ItemStack (type: diamond)
-//            ItemStack diamond = new ItemStack(Material.DIAMOND);
-//
-//            // Create a new ItemStack (type: brick)
-//            ItemStack bricks = new ItemStack(Material.BRICK);
-//
-//            // Set the amount of the ItemStack
-//            bricks.setAmount(20);
-//
-//            // Give the player our items (comma-seperated list of all ItemStack)
-//            player.getInventory().addItem(bricks, diamond);
-//        }
-//
-//        // If the player (or console) uses our command correct, we can return true
-//        return true;
-//    }
-
     @Override
     public void onDisable() {
 
     }
-
-
-
-
-
 
 
 
